fdc283fa0ea487fe7b1dcf4b799ae864eae56d56
Ishan Chowdhury <10.ishanc@gmail.com>
2025-12-18 14:05:42 +0530

    Create E4.java
    https://leetcode.com/problems/asteroid-collision/

---
+++ Week3/E4.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) 
+    {
+        Stack<Integer>st=new Stack<>();
+       
+        for(int i=0;i<asteroids.length;i++)
+        {
+            int element=asteroids[i];
+            if(element>0)
+            st.push(element);
+            else
+           {
+               while((!st.isEmpty())&&st.peek()>0&&Math.abs(element)>st.peek())
+               {
+                   
+                      st.pop();
+                   
+               }
+
+               if(st.isEmpty()||st.peek()<0)
+               st.push(element);
+          
+               if(st.peek()==-element)
+               st.pop();
+           }
+        }
+        int arr[]=new int[st.size()];
+        int len=st.size();
+        for(int i=len-1;i>=0;i--)
+        {
+            arr[i]=st.pop();
+        }
+        return arr;
+    }
+}
+https://leetcode.com/problems/asteroid-collision/
